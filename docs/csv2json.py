3f82bdcdb4f49d4999436c17433e38a8f66c485e
Aidan Holland <aidan@censys.io>
2023-04-26 17:20:38 -0400

    chore: update queries for Censys

--- docs/csv2json.py
+++ docs/csv2json.py
@@ -2,19 +2,22 @@ import os
 import csv
 import json
 
+
 def nullstr(value):
     if isinstance(value, str) and value.strip() == "":
         return None
     return value
 
+
 def csv_to_json(csv_file, json_file):
     data = []
-    with open(csv_file, 'r') as csvfile:
+    with open(csv_file, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             data.append({k: nullstr(v) for k, v in row.items()})
-    with open(json_file, 'w') as jsonfile:
+    with open(json_file, "w") as jsonfile:
         json.dump(data, jsonfile, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     csv_to_json(os.sys.argv[1], os.sys.argv[2])
